dfd13b37795c416dada77561c980256d97512e38
Raffi Hovagimian <raffi.hovagimian@gmail.com>
2018-05-02 23:23:58 -0700

    adding description

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Divider, Header } from "semantic-ui-react";
 import Account from "./Account";
 import { connect } from "react-redux";
 import * as actions from "../actions";
@@ -26,7 +26,31 @@ class Home extends Component {
     return (
       <Grid>
         <Grid.Row centered>
-          <Grid.Column width="1" />
+          <Grid.Column width="14" style={{ marginTop: "1em" }}>
+            <Header as="h1">Use Case Scenario</Header>
+            <Divider />
+            <p>
+              Ben Bitdiddle (Source) sells 100 XLM tokens to Alyssa P. Hacker
+              (Destination), under the condition that Alyssa won’t resell the
+              tokens until one year has passed. Ben doesn’t completely trust
+              Alyssa so he suggests that he holds the tokens for Alyssa for the
+              year.
+            </p>
+            <p>
+              Alyssa protests. How will she know that Ben will still have the
+              tokens after one year? How can she trust him to eventually deliver
+              them?
+            </p>
+            <p>
+              Additionally, Alyssa is sometimes forgetful. There’s a chance she
+              won’t remember to claim her tokens at the end of the year long
+              waiting period. Ben would like to build in a recovery mechanism so
+              that if Alyssa doesn’t claim the tokens, they can be recovered.
+              This way, the tokens won’t be lost forever.
+            </p>
+            <p>Note: In this example the escrow is valid for 5 minutes.</p>
+            <Divider />
+          </Grid.Column>
         </Grid.Row>
         <Grid.Row centered>
           <Grid.Column width="5">
